de01f0843bbd057d1767076c35914c2ddfd83000
Phioria <chris.baugh@gmail.com>
2024-05-29 13:48:51 -0400

    Update the scraping function to work with 4 different report options

--- gatherdata.py
+++ gatherdata.py
@@ -1,13 +1,13 @@
 from dotenv import load_dotenv
 import os
 import requests
+from typing import Literal, get_args
 
 load_dotenv()
 
 atm_username = os.getenv("ATM_USERNAME_PROD")
 atm_password = os.getenv("ATM_PASSWORD_PROD")
 auth_url = os.getenv("AUTH_URL")
-#a = os.getenv("LOGIN_URL")
 summary_url = os.getenv("SUMMARY_URL")
 
 headers = {
@@ -22,13 +22,20 @@ payload = {
   'B1': 'Login'
 }
 
+# These report IDs were pulled from the network tab when running the report in a browser
+reports = {
+  "Monthly": "289278",
+  "Quarterly": "289362",
+  "Prince": "282534",
+  "Roberts": "212997"
+}
 
-def main():
-  print("ATM_USERNAME_DEV: ", atm_username)
-  print("ATM_PASSWORD_DEV: ", atm_password)
-  print("LOGIN_URL: ", auth_url)
+# Define report Names and IDs
+_REPORTS = Literal["Monthly", "Quarterly", "Prince", "Roberts"]
 
-def gatherData():
+def getCommissionData(report_: _REPORTS = "Monthly"):
+  options = get_args(_REPORTS)
+  assert report_ in options, f"'{report_}' is not in {options}"
   session = requests.Session()
   # This line is run twice due to a strange way the target site handles logins
   session.post(auth_url, headers = headers, data = payload)
@@ -38,12 +45,9 @@ def gatherData():
     'ReportCmd': 'CustomCommand',
     'DeferQuery': 'false',
     '%24lr_override': 'true',
-    'ReportID': '289278', # This was pulled from the network tab when running the report in a browser
+    'ReportID': reports[report_], 
     'CustomCmdList': 'OpenCSVBrowser'
   }
 
   summary = session.get(summary_url, params = report_params)
   return summary
-
-if __name__ == "__main__":
-  main()
